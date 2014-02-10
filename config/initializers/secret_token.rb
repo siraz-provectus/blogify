a0cdf68b51d237855a11f26ce6f72c6888017e31
Airat Sirazutdinov <siraz@yandex.ru>
2014-02-10 10:19:26 +0400

    Build initial sceleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogify::Application.config.secret_key_base = '319823bbb55cf0ef32eda3964fabfb4eb678b6555f6174f5466e769d723f409b90ea03ab140e3b9f9740c7f714e7c018f9bf6c44185e09bd9f8c4003f64427e8'
