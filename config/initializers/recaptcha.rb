4c1424119136b101b60798286ccfa5b56304aae8
Airat Sirazutdinov <siraz@yandex.ru>
2014-02-10 17:49:50 +0400

    [ Comments ] implement captcha to comment create

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Lf8ZO4SAAAAAFEVIHgGcWeIj792JyxrKLALWwl2'
+  config.private_key = '6Lf8ZO4SAAAAAEDbyXcVqzhKCGsiiIqHNRDCrjHp'
+  config.proxy = 'http://example.com'
+end
